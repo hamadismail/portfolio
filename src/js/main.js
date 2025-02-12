02660c13cf51e25562c933159b98f1eebb8aab64
hamadismail <hamad.ismail.gub@gmail.com>
2025-02-12 12:44:47 +0600

    added-work-section-with-portfolio-filter

--- src/js/main.js
+++ src/js/main.js
@@ -13,7 +13,6 @@ const tabContents = document.querySelectorAll("[data-content]");
 tabs.forEach((tab) => {
   tab.addEventListener("click", () => {
     const target = document.querySelector(tab.dataset.target);
-    console.log(target);
 
     tabContents.forEach((tabContent) => {
       tabContent.classList.remove("skills_active");
@@ -30,8 +29,25 @@ tabs.forEach((tab) => {
 });
 
 /*=============== MIXITUP FILTER PORTFOLIO ===============*/
+let mixerPortfolio = mixitup(".work_container", {
+  selectors: {
+    target: ".work_card",
+  },
+  animation: {
+    duration: 300,
+  },
+});
 
 /*===== Link Active Work =====*/
+const linkWork = document.querySelectorAll(".work_item");
+
+function activeWork() {
+  linkWork.forEach((l) => l.classList.remove("active-work"));
+  this.classList.add("active-work");
+  console.log(linkWork);
+}
+
+linkWork.forEach((l) => l.addEventListener("click", activeWork));
 
 /*===== Work Popup =====*/
 
