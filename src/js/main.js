c20fc499a33d3944e3fbf220c031ea5d3c0efa2a
hamadismail <hamad.ismail.gub@gmail.com>
2025-02-16 15:48:04 +0600

    added-portfolio-popup-section

--- src/js/main.js
+++ src/js/main.js
@@ -50,6 +50,31 @@ function activeWork() {
 linkWork.forEach((l) => l.addEventListener("click", activeWork));
 
 /*===== Work Popup =====*/
+document.addEventListener("click", (e) => {
+  if (e.target.classList.contains("work_btn")) {
+    togglePortfolioPopup();
+    portfolioItemDetails(e.target.parentElement);
+  }
+});
+
+function togglePortfolioPopup() {
+  document.querySelector(".portfolio_popup").classList.toggle("open");
+}
+
+document
+  .querySelector(".portfolio_popup-close")
+  .addEventListener("click", togglePortfolioPopup);
+
+function portfolioItemDetails(portfolioItem) {
+  document.querySelector(".pp_thumbnail img").src =
+    portfolioItem.querySelector(".work_img").src;
+
+  document.querySelector(".portfolio_pop-subtitle span").innerHTML =
+    portfolioItem.querySelector(".work_title").innerHTML;
+
+  document.querySelector(".portfolio_pop-body").innerHTML =
+    portfolioItem.querySelector(".portfolio_item-details").innerHTML;
+}
 
 /*=============== SERVICES MODAL ===============*/
 
